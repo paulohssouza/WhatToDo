4cfa1bd17f85250ec6172976e9250fb957b66980
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2023-03-14 20:11:09 -0300

    1 - Implementation of the creation of the list of products

--- app/src/main/java/com/paulohenrique/whattodo/ProductResgistrationActivity.kt
+++ app/src/main/java/com/paulohenrique/whattodo/ProductResgistrationActivity.kt
@@ -2,6 +2,7 @@ package com.paulohenrique.whattodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.paulohenrique.whattodo.databinding.ActivityProductResgistrationBinding
 import com.paulohenrique.whattodo.databinding.ActivityTaskRegistrationBinding
 
@@ -14,5 +15,38 @@ class ProductResgistrationActivity : AppCompatActivity() {
         binding = ActivityProductResgistrationBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.txtUnitProduct.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,
+        listOf("Kg", "g", "L", "ml", "unidade", "pct"))
+        globalProduct.clear()
+
+        binding.btnInsertProduct.setOnClickListener {
+            val product = binding.txtProduct.text.toString()
+            val quantity = binding.txtQuantityProduct.text.toString().toDouble()
+            val unity = binding.txtUnitProduct.selectedItem.toString()
+
+            if (binding.txtProduct.text.isNotEmpty() && binding.txtQuantityProduct.text.isNotEmpty()) {
+                val product = Product(product, quantity, unity)
+                globalProduct.add(product)
+
+                binding.txtProduct.text.clear()
+                binding.txtQuantityProduct.text.clear()
+
+                val productAdapter = ProductAdapter(this)
+                productAdapter.addAll(globalProduct)
+                binding.listViewProduct.adapter = productAdapter
+
+            } else {
+                if (binding.txtProduct.text.isEmpty()) {
+                    binding.txtProduct.error = "Preencha o nome do produto!"
+                } else {
+                    binding.txtProduct.error = null
+                }
+                if (binding.txtQuantityProduct.text.isEmpty()) {
+                    binding.txtQuantityProduct.error = "Preencha a quantidade do produto!"
+                } else {
+                    binding.txtQuantityProduct.error = null
+                }
+            }
+        }
     }
 }
